5f3ca4a2f268b51dc3b3664d8b58bc0bfe41d1d6
Pow <ksi.seam24@gmail.com>
2026-07-31 22:11:03 +0700

    Update gateway.lua

--- gateway.lua
+++ gateway.lua
@@ -1,1 +1,14 @@
+if not game:IsLoaded() then 
+  game.Loaded:Wait() 
+end
 
+local GamesFolderUrl = "https://raw.githubusercontent.com/Yenixs/YenixExploit/refs/heads/main/Games"
+
+local BS = game.GameId == 7436755782 or game.CreatorId == 33720745
+local AE = game.GameId == 7613921865 or game.CreatorId == 35929511
+
+if BS then 
+  loadstring(game:HttpGet(GamesFolderUrl .. '/Blockspin.lua'))();
+elseif AE then
+  loadstring(game:HttpGet(GamesFolderUrl .. '/AnimeExpedition.lua'))();
+end
