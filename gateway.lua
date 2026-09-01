1811b07f87c3a687b67cfb99d7e8cc56d800d846
Pow <ksi.seam24@gmail.com>
2026-09-01 19:02:30 +0700

    Update gateway.lua

--- gateway.lua
+++ gateway.lua
@@ -6,9 +6,14 @@ local GamesFolderUrl = "https://raw.githubusercontent.com/Yenixs/YenixExploit/re
 
 local BS = game.GameId == 7436755782 or game.CreatorId == 33720745
 local AE = game.GameId == 7613921865 or game.CreatorId == 35929511
+local SE = game.GameId == 10563114921 or game.CreatorId == 825735094
 
 if BS then 
   loadstring(game:HttpGet(GamesFolderUrl .. '/Blockspin.lua'))();
 elseif AE then
   loadstring(game:HttpGet(GamesFolderUrl .. '/AnimeExpedition.lua'))();
+elseif SE then
+  loadstring(game:HttpGet(GamesFolderUrl .. '/Steal%20An%20Egg.lua'))();
 end
+
+https://raw.githubusercontent.com/Yenixs/YenixExploit/refs/heads/main/Games/Steal%20An%20Egg.lua
